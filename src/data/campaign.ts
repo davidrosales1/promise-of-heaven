c6495c1a4387e4865927dd58e7e70337057b6470
davidrosales1 <david.d.rosales1@gmail.com>
2026-06-10 12:13:58 -0700

    feat: create landing page foundation and component structure

---
+++ src/data/campaign.ts
@@ -0,0 +1,7 @@
+export const navItems = [
+  { label: "New Book", href: "#book" },
+  { label: "Heaven Concert", href: "#concert" },
+  { label: "Exclusive Interviews", href: "#interviews" },
+  { label: "Free Resources", href: "#resources" },
+  { label: "Support Turning Point", href: "#support" },
+];
